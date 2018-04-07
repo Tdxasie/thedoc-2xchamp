f039db017ee4fc908f54ef5b286c3c4301169aa3
Tdx-Chevereuil <pierre.douchka@gmail.com>
2018-04-07 14:00:43 +0200

    mass import

---
+++ doc.js
@@ -0,0 +1,175 @@
+const YoutubeStream = require('ytdl-core')
+const fetch = require("node-fetch");
+
+module.exports = class Doc {
+
+  constructor (message) {
+    if(message != undefined){
+      this.message = message
+
+      this.voiceChnl = message.guild.channels
+        .filter(function(channel) {return channel.name === 'Chevreuil'})
+        .first()
+
+    } else {
+      console.log("message not defined")
+    }
+  }
+
+  playVideo (url) {
+    this.voiceChnl.join()
+      .then(function(connection) {
+        connection
+          .playStream(YoutubeStream(url))
+          .on('end', function () {
+            connection.disconnect()
+          })
+      })
+  }
+
+  joinChevreuil () {
+    var message = this.message;
+    this.voiceChnl.join()
+      .then(function() {
+          message.channel.send("I'm here, feel my breath over your neck", {tts: true})
+      })
+  }
+
+  leave () {
+    this.voiceChnl.leave()
+  }
+
+  noWorries () {
+    this.playVideo('https://youtu.be/UbHIViE3pRE')
+  }
+
+  rAUUL () {
+    var rauuls = ["https://youtu.be/Y9-vGdWLg7w",
+                  "https://youtu.be/W3g9PyUDPxg",
+                  "https://youtu.be/TQgeO6GaPLo",
+                  "https://youtu.be/yx4xD8qV1cE",
+                  "https://youtu.be/QlI6YKDkGTE",
+                  "https://youtu.be/G38f8W_wykI",
+                  "https://youtu.be/dp1zjoJyGGQ",
+                  "https://youtu.be/P-zYLG-lSVs",
+                  "https://youtu.be/8cykZRS9yZ8",
+                  "https://youtu.be/UyIXuv3PO50"];
+
+    var random = Math.floor(Math.random() * 10);
+    this.playVideo(rauuls[random]);
+  }
+
+  tip () {
+    var message = this.message;
+    let args = message.content.split(' ')
+    let val = args[1];
+
+    switch(true) {
+      case (val < 1):
+        message.reply("<:lul:422532691817136203>");
+        break
+      case (val == 1):
+        message.reply("How dare you give me one dollar ?! <:lul:422532691817136203>");
+        break
+      case (val > 1 && val < 200):
+        message.reply("Thank you for the "+val+" $ donation");
+        break
+      case (val > 200):
+        message.reply("ARE YOU INSANE ?! "+val+"$ ??");
+        break
+      }
+    }
+
+    randomShowUp(){
+      function sleep(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+      }
+
+      async function callTheDoc(theDoc) {
+
+        var random = Math.floor((Math.random() * 121) + 30);
+
+        await sleep(random*60*1000);
+
+        if(Math.floor((Math.random() * 10) + 1) == 1){
+
+          console.log("I'm back RARE");
+          theDoc.playVideo("https://youtu.be/F_ZcIjjfh4A")
+
+        } else {
+
+          console.log("I'm back");
+          theDoc.playVideo("https://youtu.be/bhyBsKnGTZI")
+
+        }
+
+        callTheDoc(theDoc);
+      }
+
+      callTheDoc(this);
+    }
+
+    checkLive(bot) {
+      function sleep(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+      }
+      var liveDoc = true;
+      async function checkDocStream(message,bot){
+        fetch('https://api.twitch.tv/kraken/streams/drdisrespectlive?client_id=vnu4gzd7h2dasngmfkldwc1aapw2xm')
+        .then((response) => response.json())
+          .then((data) => {
+            if(data["stream"] === null){
+
+              console.log("It's a god damn snooze fest in there")
+              if(liveDoc){
+                liveDoc = false
+                bot.user.setActivity('ses Trophées', { type: 'WATCHING' })
+              }
+
+            }
+            else{
+              console.log("The Arena is wide open !")
+
+              if(!liveDoc){
+                liveDoc = true
+                // message.channel.send("The Arena is wide open with "+data["stream"].game+" !", {tts: true})  '"+data["stream"]["channel"].status+"'
+                // var mess = " -- "+data["stream"]["channel"].status+" -- \r    Playing : "+data["stream"].game+"\r    Champions : "+data["stream"].viewers*1000+"\r---------------------------------"
+                message.channel.send("The Arena is WIDE open ! "+data["stream"].viewers*1000+" Champions !\r => https://www.twitch.tv/drdisrespectlive")
+                bot.user.setActivity(data["stream"].game, {type: "STREAMING"})
+              }
+
+            }
+
+          });
+          await sleep(1000*120);
+          checkDocStream(message,bot);
+        }
+        var liveZerator = false
+        async function checkZeratorStream(message){
+          fetch('https://api.twitch.tv/kraken/streams/zerator?client_id=vnu4gzd7h2dasngmfkldwc1aapw2xm')
+            .then((response) => response.json())
+              .then((data) => {
+                if(data["stream"] === null){
+                  liveZerator = false
+                  console.log("Zerator not live")
+                }
+                else{
+                  console.log("Zerator is live")
+
+                  if(!liveZerator){
+                    liveZerator = true
+                    message.channel.send("A little french streamer is live ! He is playing "+data["stream"].game+"\r => https://www.twitch.tv/zerator")
+                  }
+
+                }
+
+              });
+            await sleep(1000*120);
+            checkZeratorStream(message);
+          }
+        checkDocStream(this.message,bot);
+        checkZeratorStream(this.message);
+      }
+
+
+  }
---
+++ index.js
@@ -0,0 +1,95 @@
+const Discord = require('discord.js')
+const bot = new Discord.Client()
+const Doc = require('./doc.js')
+// import {Doc} from './doc.js';
+var dafydoc;
+
+bot.on('ready', function () {
+  console.log("Ready to dominate")
+  dafydoc = true;
+})
+
+bot.login('NDMxODQ4MzQwOTgxMDIyNzIw.DakwQQ.UoqWc9Y9ZNFya5tZ4IMdEQglDI8')
+
+bot.on('message', message => {
+  if(dafydoc){
+    var theOnceInAWhile = new Doc(message)
+    theOnceInAWhile.randomShowUp();
+    theOnceInAWhile.checkLive(bot);
+    dafydoc=false;
+  }
+
+  var doc = new Doc(message)
+
+  switch(message.content){
+
+    case 'doc, speak':
+      message.channel.send("You are the best", {tts: true})
+      break
+
+    case 'doc, come to me':
+      doc.joinChevreuil()
+      break
+
+    case 'get your trophies':
+      doc.leave()
+      break
+
+    case "doc I'm worried":
+      doc.noWorries()
+      break
+
+    case "bonjour":
+      message.channel.send("what ?", {tts: true})
+      break
+
+    case "raul":
+      doc.rAUUL()
+      break
+
+  }
+
+  switch (true) {
+      case /<:Msmile:428299610272956430>/.test(message.content):
+        message.channel.send("Did you miss me ?", {tts: true})
+        break;
+
+      case /mdr|lol|ptdr|xD|x\)|riz|<:lol:431951388751298561>|<:noel:416549332205043712>/.test(message.content):
+        message.channel.send("<:lul:422532691817136203>")
+        break;
+
+      case /<:top1:427923720481275904>/.test(message.content):
+        message.channel.send("Well done kids", {tts: true})
+        break;
+
+      case /wtf/.test(message.content):
+        message.channel.send("<:wat:336130868869332994>")
+        break;
+
+      case (message.content.startsWith('$tip')):
+        doc.tip()
+        break;
+
+      case /(thx|thanks|merci)(?=\sdoc)/.test(message.content):
+        message.reply("Your welcome kiddo \r https://imgur.com/a/KEldC")
+        break
+    }
+})
+
+
+
+
+
+// bot.on('message', message => {
+//   if (message.content === 'doc, come to me') {
+//     let voiceChannel = message.guild.channels
+//       .filter(function (channel) { return channel.name === 'Chevreuil' })
+//       .first()
+//     voiceChannel
+//       .join()
+//       .then(function (connection){
+//         message.channel.send('/tts Im here, feel my breath over your neck')
+//         connection.disconnect()
+//       })
+//   }
+// })
